b4186a28c66e4e5ee72feb81d936b3a4beb1c9ff
Astro <88717385+astrogames06@users.noreply.github.com>
2024-06-01 16:02:13 +1000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <raylib.h>
+#include <raymath.h>
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include <cstdlib>
+
+#if defined(PLATFORM_WEB)
+    #include <emscripten/emscripten.h>
+#endif
+
+
+const int WIDTH = 850;
+const int HEIGHT = 450;
+
+void UpdateDrawFrame();
+
+int main(void)
+{
+	InitWindow(WIDTH, HEIGHT+50, "Pixel Art! :)");
+
+	#if defined(PLATFORM_WEB)
+    	emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
+	#else
+		SetTargetFPS(60);   // Set our game to run at 60 frames-per-second
+		//--------------------------------------------------------------------------------------
+
+		// Main game loop
+		while (!WindowShouldClose())    // Detect window close button or ESC key
+		{
+			UpdateDrawFrame();
+		}
+	#endif
+
+	CloseWindow();
+
+	return 0;
+}
+
+void UpdateDrawFrame()
+{
+	BeginDrawing();
+	
+	ClearBackground(RAYWHITE);
+
+	DrawText("Yay you setup a raylib web game!", 100, 100, 20, BLACK);
+
+	EndDrawing();
+}
